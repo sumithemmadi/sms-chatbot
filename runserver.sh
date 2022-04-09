f30606f455fd2c8a30d6c57fc3423025e16494f6
sumithemmadi <sumithemmadi244@gmail.com>
2022-04-09 19:23:48 +0530

    Updated

--- runserver.sh
+++
@@ -1,4 +0,0 @@
-export FLASK_APP=server.py
-set FLASK_APP=server.py
-
-flask run
--- server.py
+++ server.py
@@ -3,8 +3,12 @@ import json
 import subprocess as sp
 from dotenv import load_dotenv
 from google.cloud import dialogflow_v2beta1 as dialogflow
-
+import uuid
+  
 load_dotenv()
+projectId = os.getenv("PROJECTID")
+sessionId =  uuid.uuid4()
+
 
 print("loading contacts")
 os.system("termux-contact-list > contacts.json")
@@ -14,25 +18,31 @@ with open('contacts.json', 'r') as contactList:
 for contact in contactList:
     print(len(contact))
 
-
-projectId = "athena-lvsbrh"
-sessionId = "123456"
-
-
 while 1 == 1:
     output = sp.getoutput('termux-notification-list')
     jsonData = json.load(output)
+
     for notification in jsonData:
         if notification['packageName'] == "com.termux":
+            number = notification['title']
+            text = notification['content']
+            ID = notification['id']
+
+            #Find Real Number
+            if number[0] != '+':
+                for contact in contactList:
+                    if contact['name'] == number:
+                        number = contact['number']
+
             sessionClient = dialogflow.SessionsClient()
             session = sessionClient.session_path(projectId, sessionId)
-            textInput = dialogflow.TextInput(text="hi", language_code="en_US")
+            textInput = dialogflow.TextInput(text, language_code="en_US")
             queryInput = dialogflow.QueryInput(text=textInput)
             response = sessionClient.detect_intent(
                 request={"session": session, "query_input": queryInput})
-            print("Fulfillment text: {}\n".format(
-                response.query_result.fulfillment_text))
-            sms_sent_status = sp.getoutput('termux-sms-send -n ')
+            print(" {}".format(response.query_result.fulfillment_text))
+            sms_sent_status = sp.getoutput('termux-sms-send -n '+ number + " {}".format(response.query_result.fulfillment_text))
+            os.system("termux-notification-remove  {}".format(ID))
             print("SMS sent successfully")
     
     output.close()
