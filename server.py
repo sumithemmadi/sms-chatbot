70d4ada77220932336879c367026282c4fdb9f52
sumithemmadi <sumithemmadi244@gmail.com>
2022-04-09 12:14:21 +0530

    Updated

--- server.py
+++ server.py
@@ -1,18 +1,7 @@
-from flask import Flask
 import subprocess as sp
 import json
 
 
 output = sp.getoutput('termux-notification-list')
 jsonOutput = json.loads(output)
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-
-    return output[0]
-
-
-if __name__ == "__main__":
-    app.run()
+print(output[0])
