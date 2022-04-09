f4dabcde04520202b62b0f79b28a1edbe6589c9c
sumithemmadi <sumithemmadi244@gmail.com>
2022-04-09 18:44:51 +0530

    Updated

--- server.py
+++ server.py
@@ -4,11 +4,15 @@ import subprocess as sp
 from dotenv import load_dotenv
 from google.cloud import dialogflow_v2beta1 as dialogflow
 
+load_dotenv()
 
-output = sp.getoutput('termux-notification-list')
-# output = open("list.json")
+print("loading contacts")
+os.system("termux-contact-list > contacts.json")
+with open('contacts.json', 'r') as contactList:
+  data = json.load(contactList)
 
-jsonData = json.load(output)
+for contact in contactList:
+    print(len(contact))
 
 
 projectId = "athena-lvsbrh"
@@ -16,15 +20,19 @@ sessionId = "123456"
 
 
 while 1 == 1:
+    output = sp.getoutput('termux-notification-list')
+    jsonData = json.load(output)
     for notification in jsonData:
         if notification['packageName'] == "com.termux":
             sessionClient = dialogflow.SessionsClient()
             session = sessionClient.session_path(projectId, sessionId)
             textInput = dialogflow.TextInput(text="hi", language_code="en_US")
             queryInput = dialogflow.QueryInput(text=textInput)
-            response = sessionClient.detect_intent(request={"session": session, "query_input": queryInput})
-            print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
-            sms_sent_status = sp.getoutput('termux-notification-list')
+            response = sessionClient.detect_intent(
+                request={"session": session, "query_input": queryInput})
+            print("Fulfillment text: {}\n".format(
+                response.query_result.fulfillment_text))
+            sms_sent_status = sp.getoutput('termux-sms-send -n ')
             print("SMS sent successfully")
-
-output.close()
+    
+    output.close()
